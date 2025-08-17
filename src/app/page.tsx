c5d9d4caa0c8565699703001bea596cd0fd8b735
thisisatulkumar <theatulkumar11@gmail.com>
2025-08-17 09:43:37 +0530

    Design the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,39 @@ import { useState, useEffect, useRef } from 'react';
 
 import { io, Socket } from 'socket.io-client';
 
+import {
+    Mic,
+    MicOff,
+    Video,
+    VideoOff,
+    LayoutGrid,
+    MessageSquare,
+    Presentation
+} from 'lucide-react';
+import { ImPhoneHangUp } from "react-icons/im";
+
 const SIGNALING_SERVER = 'http://localhost:5000';
 const STUN_SERVERS = [
     {
         urls: 'stun:stun.l.google.com:19302'
     }
 ];
+
+const displayTime = () => {
+    const date = new Date();
+
+    const hours = date.getHours();
+    let minutes:string | number = date.getMinutes();
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    if (hours > 12) {
+        return `${hours - 12}:${minutes} PM`;
+    }
+    return `${hours}:${minutes} AM`;
+}
 
 export default function Home() {
     const [roomId, setRoomId] = useState<string>('');
@@ -99,7 +126,7 @@ export default function Home() {
 
             if (localVideoRef.current) {
                 localVideoRef.current.srcObject = stream;
-                localVideoRef.current.muted = true;
+                localVideoRef.current.muted = false;
             }
 
             return stream;
@@ -235,9 +262,44 @@ export default function Home() {
     }, []);
 
     return (
-        <div style={{ padding: 12, fontFamily: "Arial, sans-serif" }}>
-            <div style={{ display: "flex", gap: 16 }}>
-                <div>
+        <div className="h-screen flex flex-col bg-[#181A1B]">
+
+            {/* Body */}
+            <div className="h-[90vh] flex justify-center items-center w-screen p-5">
+                <div className="w-[50%] p-2.5 h-full">
+                    <h1 className="text-[#E2DFDB]">Local</h1>
+                    <video 
+                        className="rounded-md w-full h-full bg-black"
+                        ref={localVideoRef} 
+                        autoPlay 
+                        playsInline 
+                        muted 
+                    />
+                </div>
+                
+                <div className="w-[50%] p-2.5 h-full">
+                    <h1 className="text-[#E2DFDB]">Remote</h1>
+                    <video 
+                        className="rounded-md w-full h-full bg-black"
+                        ref={remoteVideoRef} 
+                        autoPlay 
+                        playsInline 
+                        muted 
+                    />
+                </div>
+            </div>
+            
+            {/* Footer */}
+            <div className="flex justify-center items-center h-[10vh] px-5">
+
+                {/* Info */}
+                <div className="info w-[25%] pl-3.5 text-gray-100">
+                    <span className="font-semibold">Joined at: </span>
+                    <span>{displayTime()}</span>
+                </div>
+
+                {/* Control Buttons */}
+                <div className="controls w-[50%] flex items-center justify-around">
                     <input 
                         value={roomId}
                         onChange={event => setRoomId(event.target.value)}
@@ -255,41 +317,35 @@ export default function Home() {
 
                     <button
                         onClick={muteUnmuteAudio}
+                        className="bg-[#464D5A] hover:bg-[#6A7282] h-[5vh] w-[8vh] flex justify-center items-center rounded-md cursor-pointer transition-all duration-300 text-[#E2DFDB]"
                     >
-                        Mute
+                        {
+                            audioEnabled ? <Mic /> : <MicOff />
+                        }
                     </button>
 
                     <button
                         onClick={showHideVideo}
+                        className="bg-[#464D5A] hover:bg-[#6A7282] h-[5vh] w-[8vh] flex justify-center items-center rounded-md cursor-pointer transition-all duration-300 text-[#E2DFDB]"
                     >
-                        Show/Hide 
+                        {
+                            videoEnabled ? <Video /> : <VideoOff />
+                        }
                     </button>
 
                     <button
                         onClick={hangUpCall}
+                        className="bg-[#900003] hover:bg-[#E7000B] h-[5vh] w-[8vh] flex justify-center items-center rounded-md cursor-pointer transition-all duration-300  text-[#E2DFDB]"
                     >
-                        Hang Up
+                        <ImPhoneHangUp />
                     </button>
                 </div>
-
-                <div>
-                    <h1>Local</h1>
-                    <video 
-                        ref={localVideoRef} 
-                        autoPlay 
-                        playsInline 
-                        muted 
-                        style={{ width: 300, background: "#000" }} 
-                    />
-
-                    <h1>Remote</h1>
-                    <video 
-                        ref={remoteVideoRef} 
-                        autoPlay 
-                        playsInline 
-                        muted 
-                        style={{ width: 300, background: "#000" }} 
-                    />
+                
+                {/* Utility Buttons */}
+                <div className="w-[25%] flex justify-end items-center pr-3.5 text-[#E2DFDB]">
+                    <LayoutGrid className="mx-5" />
+                    <Presentation className="mx-5" />
+                    <MessageSquare className="mx-5" />
                 </div>
             </div>
         </div>
